d2d861d3e77a445c2fda09e7fdd909117ce931ca
Qingying Meng <qmeng222@gmail.com>
2024-01-16 15:42:40 -0800

    defined a class that inherits from LLMChain with a  method to return a generator

--- test.py
+++ test.py
@@ -10,7 +10,8 @@ load_dotenv() # load environment variables from .env
 class StreamingHandler(BaseCallbackHandler):
     # define a callback method called `on_llm_new_token` which takes a token as an argument and prints it:
     def on_llm_new_token(self, token, **kwargs): # **kwargs indicates that it can accept additional keyword arguments (although they might not be used in this snippet)
-        print(token)
+        # print(token)
+        pass
 
 # create an instance of the ChatOpenAI class:
 chat = ChatOpenAI(
@@ -23,13 +24,28 @@ prompt = ChatPromptTemplate.from_messages([ # a list of tuples
     ("human", "{content}") # {content} placeholder
 ])
 
-chain = LLMChain(llm=chat, prompt=prompt)
+# define a class called StreamingChain that extends/inherits from LLMChain (inherits functionality from the LLMChain class):
+class StreamingChain(LLMChain):
+    # define a `stream` method to return a generator (a generator function produces values one at a time using the yield keyword):
+    def stream(self, input): # `self` refers to the instance of the class
+        print("🌈", self(input)) # the res of calling the chain
+        yield 'hi'
+        yield 'there'
+# now any instance of the StreamingChain class has a `stream` method
 
-# NOTE: LLMs are happy to stream, but chains are not!
-output = chain.stream(input={"content": "Tell me a joke."})
-# print("✅", output)
-for msg in output:
-  print("👋", msg)
+chain = StreamingChain(llm=chat, prompt=prompt) # an instance of the StreamingChain class
+
+# the `chain` obj has the `stream` method:
+for output in chain.stream(input={"content": "tell me a joke"}):
+    print(output)
+
+# chain = LLMChain(llm=chat, prompt=prompt)
+
+# # NOTE: LLMs are happy to stream, but chains are not!
+# output = chain.stream(input={"content": "Tell me a joke."})
+# # print("✅", output)
+# for msg in output:
+#   print("👋", msg)
 
 # # use the template to create an actual conversation by filling in the {content} placeholder with the specific content (format the template):
 # messages = prompt.format_messages(content="Tell me a joke.")
